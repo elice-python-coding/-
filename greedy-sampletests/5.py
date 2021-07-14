9dda94202060578a3a6b142e512f56a82d025570
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-07-15 00:13:18 +0900

    Update 5.py

--- greedy-sampletests/5.py
+++ greedy-sampletests/5.py
@@ -1,17 +1,31 @@
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
+# n, m = map(int, input().split())
+# data = list(map(int, input().split()))
 
-# 1부터 10까지의 무게를 담을 수 있는 리스트
-array = [0] * 11
+# # 1부터 10까지의 무게를 담을 수 있는 리스트
+# array = [0] * 11
 
-for x in data:
-    # 각 무게에 해당하는 볼링공의 개수 카운트
-    array[x] += 1
+# for x in data:
+#     # 각 무게에 해당하는 볼링공의 개수 카운트
+#     array[x] += 1
 
-result = 0
-# 1부터 m까지의 각 무게에 대하여 처리
-for i in range(1, m + 1):
-    n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
-    result += array[i] * n # B가 선택하는 경우의 수와 곱해주기
+# result = 0
+# # 1부터 m까지의 각 무게에 대하여 처리
+# for i in range(1, m + 1):
+#     n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+#     result += array[i] * n # B가 선택하는 경우의 수와 곱해주기
 
-print(result)
+# print(result)
+
+nums = list(map(int, input().split()))
+weight = list(map(int, input().split()))
+
+count = 0
+
+for i in range(len(weight)-1):
+    for j in range(i+1, len(weight)):
+        if weight[j] == weight[i]:
+            pass
+        else:
+            count += 1
+
+print(count)
