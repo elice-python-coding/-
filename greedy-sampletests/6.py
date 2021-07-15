bfdd0b12afb34692a07b843bc3332ae3d5936431
은선 남궁 <nammugmuhg@gmail.com>
2021-07-15 13:09:05 +0900

    let's learn datastructure

--- greedy-sampletests/6.py
+++ greedy-sampletests/6.py
@@ -1,23 +1,24 @@
-food_times = list(map(int, input().split()))
-k = int(input())
-position_change = 0
+def solution(food_times, k):
+    position_change = 0
+    sum_1 = sum(food_times)
+    if sum_1 <= k:
+        return -1
 
-while k != 0:
-    now = food_times[0]
-    position_change += 1
-    if int(now) > 0:
-        food_times[0] = food_times[0] - 1
-        temp = food_times[0]
-        del food_times[0]
-        food_times.append(temp)
-        k -= 1
-        print(k, food_times)
+    while k != -1:
+        now = food_times[0]
+        position_change += 1
+        if now > 0:
+            food_times[0] = food_times[0] - 1
+            temp = food_times[0]
+            del food_times[0]
+            food_times.append(temp)
+            k -= 1
+        else:
+            temp = food_times[0]
+            del food_times[0]
+            food_times.append(temp)
+    ret = position_change % len(food_times)
+    if ret == 0:
+        return len(food_times)
     else:
-        temp = food_times[0]
-        del food_times[0]
-        food_times.append(temp)
-
-
-print(position_change%len(food_times)+1,"end!")
-
-#다시풀기
+        return ret
