5f7df6b3f7299e5819cff99e95375c0524a87e7e
은선 남궁 <nammugmuhg@gmail.com>
2021-07-20 13:17:37 +0900

    deque

--- greedy-sampletests/6.py
+++ greedy-sampletests/6.py
@@ -1,53 +1,24 @@
-# food_times = list(map(int,input().split()))
-# k = int(input())
-# position_change = 0
-# sum_1 = sum(food_times)
-# if sum_1 <= k:
-#     print(-1)
-# i = 0
-# length = len(food_times)
-# while k != -1:
-#     if i > length:
-#         i = i % length - 1
-#         print(i)
-#     now = food_times[i]
-#     print('now:',now)
-#     if now > 0:
-#         food_times[i] = food_times[i] - 1
-#         k -= 1
-#     i += 1
-#     position_change += 1
-#
-#
-# ret = position_change % length
-# if ret == 0:
-#     print(length)
-# else:
-#     print(ret)
-#
+from collections import deque
+
+
 def solution(food_times, k):
+    a = deque(food_times)
     position_change = 0
     sum_1 = sum(food_times)
     if sum_1 <= k:
         return -1
 
     while k != -1:
-        now = food_times[0]
+        now = a[0]
         position_change += 1
         if now > 0:
-            food_times[0] = food_times[0] - 1
-            temp = food_times[0]
-            del food_times[0]
-            food_times.append(temp)
+            a[0] = a[0] - 1
+            a.append(a.popleft())
             k -= 1
         else:
-            temp = food_times[0]
-            del food_times[0]
-            food_times.append(temp)
+            a.append(a.popleft())
     ret = position_change % len(food_times)
     if ret == 0:
         return len(food_times)
     else:
         return ret
-
-##자료구조 마스터하고 풀것. 레벨4다..
